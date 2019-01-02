c7b5c525d70d82fc38babfb00112b577174ffa8b
tortoise1992 <772084015@qq.com>
2019-01-02 09:19:38 +0800

    pageoffice案例

--- src/pages/others/pageOffice/index.js
+++ src/pages/others/pageOffice/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { Breadcrumb, Card, Row, Col,Button} from 'antd';
 let POBrowser
-class PageOffice extends Component {    
+
+class PageOffice extends Component {  
+    constructor(){
+        super()
+        PageOffice.test=this.test.bind(this)  //方法一，将静态方法绑定到class上
+    }  
     componentDidMount() {       
         POBrowser=window.POBrowser
+        // window.__react=this   //方法二，将组件实例绑定到window上面
     }
     openWord=()=>{
-        POBrowser.openWindowModeless('http://192.168.102.119:8001/word?name=zhouqinghui','width=1200px;height=800px;')
-    }    
-    render() {
+        POBrowser.openWindowModeless('http://192.168.102.171:8080/word','width=1200px;height=800px;')
+    }   
+    test(name){
+        console.log(name)
+    }
+    render() {        
         return (
             <React.Fragment>
                 <Breadcrumb  className="breadcrumb">
@@ -30,4 +39,8 @@ class PageOffice extends Component {
     }
 }
 
+window.refresh=function(name){
+    // console.log(window.__react.test())
+    console.log(PageOffice.test(name))
+}
 export default PageOffice;
